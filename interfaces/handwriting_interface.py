04bbc72463e8667a0e83c9d657f6291ce3eb5123
Martin Mayr <martin.mayr@fau.de>
2023-01-21 10:23:11 +0100

    improved word splitting a bit.

--- interfaces/handwriting_interface.py
+++ interfaces/handwriting_interface.py
@@ -1,10 +1,11 @@
 import os
 import random
 import sys
+sys.path.append(os.path.join(os.getcwd(),"..","HandwritingTransformers"))
+from torchvision.utils import make_grid
 
 from HandwritingTransformers.data.dataset import get_transform
 
-sys.path.append(os.path.join(os.getcwd(),"..","HandwritingTransformers"))
 import numpy as np
 import torch
 from torchvision import transforms
@@ -28,14 +29,37 @@ def load_model():
     print(MODEL_PATH + ' : Model loaded Successfully')
     return model
 
+def word_splitter(img, random=True, right_padding=50, img_width=192):
+    # TODO: add blurring
+    offset = 5
+    img = 1-img
+    t = torch.zeros(size=(1,img.shape[1],img.shape[-1]+offset))
+    t[:,:,offset:] = img
+    occ = torch.sum(t,dim=1)
+    occ = 1- (occ / torch.max(occ))
+    if random:
+        occ = (occ * torch.rand(size=occ.shape)).squeeze()
+    start_position = torch.argmax(occ[:-right_padding])
+
+    occ_end = torch.sum(t,dim=1)[:,start_position:start_position+img_width]
+    occ_end = 1- (occ_end/torch.max(occ_end))
+    if random:
+        occ_end = (occ_end*torch.rand(size=occ_end.shape)).squeeze()
+    end_position = torch.argmax(occ_end[right_padding:])+start_position
+    print(end_position-start_position)
+    out_img = torch.ones(size=(1,32,img_width))
+    new_content = 1-img[:,:,start_position:end_position]
+    out_img[:,:,:new_content.shape[-1]] = new_content
+    return out_img
+
 def create_tensor_primings(line, n_patches=15, width=192):
-    line = normalization_transform(line)
+    line = toTensor(line)
     # line = toTensor(line)
     out = torch.empty((0,))
     for _ in range(n_patches):
-        x_position = np.random.randint(0,line.shape[-1]-width)
-        new_tensor = line[:,:,x_position:x_position+192]
-        out = torch.cat([out,new_tensor])
+        # new_tensor = torch.ones(size=(1,32,192))
+        new_tensor = word_splitter(line)
+        out = torch.cat([out,normalization_transform(toPIL(new_tensor))])
     return out.unsqueeze(0)
 
 def transform_img():
@@ -48,3 +72,5 @@ if __name__ == "__main__":
     img = Image.open("../files/examples/0002-1-1.png")
     img = fix_height(img.convert("L"))
     tensor = create_tensor_primings(img)
+    tensor_img = make_grid(tensor.permute(1,0,2,3))
+    toPIL(tensor_img).show()
